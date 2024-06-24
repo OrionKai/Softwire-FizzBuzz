765393a60724e620c4538d67afffdef4a6ac0d31
Kai Antar <Kai.Antar@softwire.com>
2024-06-24 16:48:01 +0100

    Add code for FizzBuzz part 1

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -1,5 +1,19 @@
 package com.example.fizzbuzz
 
+fun fizzBuzz(max: Int) {
+    for (num in 1..max) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            println("FizzBuzz")
+        } else if (num % 3 == 0) {
+            println("Fizz")
+        } else if (num % 5 == 0) {
+            println("Buzz")
+        } else {
+            println(num)
+        }
+    }
+}
+
 fun main() {
-    print("Hello world!")
+    fizzBuzz(100)
 }
