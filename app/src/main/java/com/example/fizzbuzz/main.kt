e8054b3614781c4d2e6bfec6ed824ae962a7287d
Kai Antar <Kai.Antar@softwire.com>
2024-06-24 17:25:19 +0100

    Add code for Part 2

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -2,18 +2,40 @@ package com.example.fizzbuzz
 
 fun fizzBuzz(max: Int) {
     for (num in 1..max) {
-        if (num % 3 == 0 && num % 5 == 0) {
-            println("FizzBuzz")
-        } else if (num % 3 == 0) {
-            println("Fizz")
-        } else if (num % 5 == 0) {
-            println("Buzz")
-        } else {
-            println(num)
+        val msgsToPrint = mutableListOf<String>()
+
+        if (num % 3 == 0) {
+            msgsToPrint.add("Fizz")
         }
+        if (num % 5 == 0) {
+            msgsToPrint.add("Buzz")
+        }
+        if (num % 7 == 0) {
+            msgsToPrint.add("Bang")
+        }
+        if (num % 11 == 0) {
+            msgsToPrint.clear()
+            msgsToPrint.add("Bong")
+        }
+        if (num % 13 == 0) {
+            var indexOfFirstB = msgsToPrint.indexOfFirst { it.startsWith("B") }
+            if (indexOfFirstB == -1) {
+                indexOfFirstB = 0
+            }
+            msgsToPrint.add(indexOfFirstB, "Fezz")
+        }
+        if (num % 17 == 0) {
+            msgsToPrint.reverse()
+        }
+
+        if (msgsToPrint.isEmpty()) {
+            msgsToPrint.add(num.toString())
+        }
+
+        println(msgsToPrint.joinToString(""))
     }
 }
 
 fun main() {
-    fizzBuzz(100)
+    fizzBuzz(255)
 }
