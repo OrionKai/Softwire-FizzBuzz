4837c7966ec6ff482dbb2e717846082be5b6cb58
Kai Antar <Kai.Antar@softwire.com>
2024-06-24 17:30:41 +0100

    Add separate determineFizzBuzz function

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -1,38 +1,43 @@
 package com.example.fizzbuzz
 
+fun determineFizzBuzz(num: Int): String {
+    val msgsToPrint = mutableListOf<String>()
+
+    if (num % 3 == 0) {
+        msgsToPrint.add("Fizz")
+    }
+    if (num % 5 == 0) {
+        msgsToPrint.add("Buzz")
+    }
+    if (num % 7 == 0) {
+        msgsToPrint.add("Bang")
+    }
+    if (num % 11 == 0) {
+        msgsToPrint.clear()
+        msgsToPrint.add("Bong")
+    }
+    if (num % 13 == 0) {
+        var indexOfFirstB = msgsToPrint.indexOfFirst { it.startsWith("B") }
+        if (indexOfFirstB == -1) {
+            indexOfFirstB = 0
+        }
+        msgsToPrint.add(indexOfFirstB, "Fezz")
+    }
+    if (num % 17 == 0) {
+        msgsToPrint.reverse()
+    }
+
+    if (msgsToPrint.isEmpty()) {
+        msgsToPrint.add(num.toString())
+    }
+
+    return msgsToPrint.joinToString("")
+}
+
 fun fizzBuzz(max: Int) {
     for (num in 1..max) {
-        val msgsToPrint = mutableListOf<String>()
-
-        if (num % 3 == 0) {
-            msgsToPrint.add("Fizz")
-        }
-        if (num % 5 == 0) {
-            msgsToPrint.add("Buzz")
-        }
-        if (num % 7 == 0) {
-            msgsToPrint.add("Bang")
-        }
-        if (num % 11 == 0) {
-            msgsToPrint.clear()
-            msgsToPrint.add("Bong")
-        }
-        if (num % 13 == 0) {
-            var indexOfFirstB = msgsToPrint.indexOfFirst { it.startsWith("B") }
-            if (indexOfFirstB == -1) {
-                indexOfFirstB = 0
-            }
-            msgsToPrint.add(indexOfFirstB, "Fezz")
-        }
-        if (num % 17 == 0) {
-            msgsToPrint.reverse()
-        }
-
-        if (msgsToPrint.isEmpty()) {
-            msgsToPrint.add(num.toString())
-        }
-
-        println(msgsToPrint.joinToString(""))
+        val msgToPrint = determineFizzBuzz(num)
+        println(msgToPrint)
     }
 }
 
